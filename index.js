936f0f9f54e9f2ca51439488f40125358e689b69
MMariamidze <104030318+MMariamidze@users.noreply.github.com>
2022-05-30 00:26:37 +0400

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+var cinema ={
+   names:['Cavea','Amirani','Rustaveli'],
+   languages:['Eng','Geo'],
+   movies:[
+        {name:'"Closer"',imdb:8.7},
+        {name:'"The dark knight"',imdb:9.0},
+        {name:'"The platform"',imdb:8.6}
+   ]
+}
+document.getElementById("cinema-1").innerHTML="Cinema:"+" "+cinema.names[0]+"<br>" + "Language:"+" " +cinema.languages[1]+"<br>"+ "Movie:"+" "+cinema.movies[0].name+"<br> " + "IMDb:" +" "+cinema.movies[0].imdb;
+document.getElementById("cinema-2").innerHTML="Cinema:"+" "+cinema.names[1]+"<br>" + "Language:"+" " +cinema.languages[0]+"<br>"+ "Movie:"+" "+cinema.movies[1].name+"<br> " + "IMDb:" +" "+cinema.movies[1].imdb;
+document.getElementById("cinema-3").innerHTML="Cinema:"+" "+cinema.names[2]+"<br>" + "Language:"+" " +cinema.languages[1]+"<br>"+ "Movie:"+" "+cinema.movies[2].name+"<br> " + "IMDb:" +" "+cinema.movies[2].imdb;
